7d0c6594b31ab2213c5c64a1e1ba3969323a893e
hsanik <shak.iu0@gmail.com>
2025-09-19 14:33:50 +0600

    feat: add gallery section and title

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { doctors } from '@/lib/doctors';
 
 const Gallery = () => {
     return (
-        <div>
-            Gallery Component
-        </div>
+        <section className="py-12 bg-gray-50 sm:py-16 lg:py-20">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl">Meet Our Team</h2>
+                    <p className="max-w-3xl mx-auto mt-4 text-lg font-normal text-gray-600 sm:mt-5">
+                        Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo non habent claritatem insitamconsequat duis
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 };
 
