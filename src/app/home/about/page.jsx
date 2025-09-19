0b2194aecaceb98bbdfe1fd6813d1790dc32b5bc
mdiffatbari <iffatbaribappy1999@gmail.com>
2025-09-19 19:33:26 +0600

    color modify3

--- src/app/home/about/page.jsx
+++ src/app/home/about/page.jsx
@@ -26,7 +26,7 @@ const page = () => {
               </h3>
               <ul className="mt-5 space-y-4 text-[#333333] dark:text-white">
                 <li className="flex items-start gap-3">
-                  <span className="w-3 h-3 bg-[#4c69c6] rounded-full mt-2"></span>
+                  <span className="w-3 h-3 bg-[#4c69c6] rounded-full mt-2 dark:text-white"></span>
                   Personalized health and fitness insights tailored to your goals.
                 </li>
                 <li className="flex items-start gap-3">
