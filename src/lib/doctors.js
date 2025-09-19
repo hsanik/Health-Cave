79b962a4ba2706555d13e0a1c76fdf69d0675dd9
hsanik <shak.iu0@gmail.com>
2025-09-19 14:33:44 +0600

    feat: add doctors data

---
+++ src/lib/doctors.js
@@ -0,0 +1,38 @@
+export const doctors = [
+    {
+        id: 1,
+        name: 'Steven Elliott',
+        specialty: 'Cardiac Surgery',
+        image: '/images/doc1.png',
+    },
+    {
+        id: 2,
+        name: 'Dr. Michael Smith',
+        specialty: 'Cardiology',
+        image: '/images/doc2.jpg',
+    },
+    {
+        id: 3,
+        name: 'Nicole Dixon',
+        specialty: 'Gynecology',
+        image: '/images/doc3.png',
+    },
+    {
+        id: 4,
+        name: 'Dr. Hannah Wilson',
+        specialty: 'Psychiatrist',
+        image: '/images/doc4.png',
+    },
+    {
+        id: 5,
+        name: 'Jessica Miller',
+        specialty: 'Orthopedics',
+        image: '/images/doc5.png',
+    },
+    {
+        id: 6,
+        name: 'Michael Clark',
+        specialty: 'Dermatology',
+        image: '/images/doc6.png',
+    },
+];
