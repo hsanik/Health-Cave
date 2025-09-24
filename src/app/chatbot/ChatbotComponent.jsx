fbb2d4fac2f3c741d3220a98337c371d0f6c39c6
hsanik <shak.iu0@gmail.com>
2025-09-24 22:29:21 +0600

    feat: create main chatbot component

---
+++ src/app/chatbot/ChatbotComponent.jsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
+
+import config from './config.js';
+import MessageParser from './MessageParser.js';
+import ActionProvider from './ActionProvider.js';
+
+const ChatbotComponent = () => {
+  return (
+    <div>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
+    </div>
+  );
+};
+
+export default ChatbotComponent;
